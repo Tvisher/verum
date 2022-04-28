1c5cfc0c2682d0962adac526d9d661024bcd96b8
Tvisher <tvisher@mail.ru>
2022-04-28 11:00:32 +0600

    add animation

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import './vendors/vendors.js';
 import Swiper, {
     Navigation,
     Pagination,
-    Thumbs,
+    Autoplay,
     EffectFade,
     Mousewheel
 } from 'swiper';
@@ -48,8 +48,8 @@ baseFunction.testWebP();
 
 
 const fullscreenSlider = new Swiper('.fullscreen-slider', {
-    modules: [Pagination, EffectFade],
-    speed: 800,
+    modules: [Pagination, EffectFade, Autoplay],
+    speed: 1200,
     slidesPerView: 1,
     effect: 'fade',
     grabCursor: true,
@@ -58,9 +58,18 @@ const fullscreenSlider = new Swiper('.fullscreen-slider', {
     fadeEffect: {
         crossFade: true
     },
+    autoplay: {
+        delay: 6000,
+    },
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
+    },
+    on: {
+        resize(swiper) {
+            swiper.updateAutoHeight(300);
+            swiper.update();
+        }
     },
     breakpoints: {
         768: {
@@ -70,3 +79,25 @@ const fullscreenSlider = new Swiper('.fullscreen-slider', {
 });
 
 
+
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 80, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
