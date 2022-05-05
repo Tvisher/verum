ccc8f19f70b67177854b54bc63e4452a55c5400f
Tvisher <tvisher@mail.ru>
2022-05-05 17:27:49 +0600

    animation fix

--- src/js/app.js
+++ src/js/app.js
@@ -177,9 +177,9 @@ AOS.init({
     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 25, // offset (in px) from the original trigger point
-    delay: 100, // values from 0 to 3000, with step 50ms
-    duration: 1200, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 800, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out', // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
